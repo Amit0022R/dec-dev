d3062567e2e2f31d38d0ce9ac8cb7e99bcffc705
Amit Kumar <144479526+Amit0022R@users.noreply.github.com>
2025-12-22 23:20:12 +0530

    js string

---
+++ js/part1/string.js
@@ -0,0 +1,22 @@
+// const str1 = "Hello, World!";
+// const str2 = 'JavaScript is fun.';
+// const str3 = `Template literals are great for embedding expressions: ${2 + 2}`;
+// console.log(str1);
+// console.log(str1.length); // space count too
+// console.log(str2);
+// console.log(str3);
+
+
+const s = "Dsa is very tough for online assessments";
+// const a = s.toUpperCase();
+// console.log(a);
+
+// console.log(s.slice(4, 6)); // is
+// console.log(s.slice(-13, -5)); // assessme
+
+
+// console.log(s.substring(4, 6)); // is
+// console.log(s.substring(-13, -5)); // Empty string because substring treats negative as 0
+
+// console.log(s.substr(4, 2)); // is
+// console.log(s.substr(-13, 8)); // assessme
